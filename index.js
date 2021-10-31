a811cce49891cf8c5650441f82bdf2932934bb48
John Malcolm Anderson <hi@johnmalcolmdesign.com>
2021-10-31 13:58:29 +0200

    Saving image on server

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 const httpServer = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("./images"));
 //app.get("/images", express.static(path.join(__dirname, "./images")));
@@ -14,6 +17,32 @@ app.use(express.static("./images"));
 app.get('/drawing.html', (req, res) => {
   res.sendFile(path.join(__dirname, './drawing.html'));
 });
+
+const uploadImage = async (req, res, next, data) => {
+
+  try {
+
+      // to declare some path to store your converted image
+      const path = './images/'+Date.now()+'.png'
+
+      console.log(req.body.base64image)
+      const imgdata = req.body.base64image;
+
+      // to convert base64 format into random filename
+      const base64Data = imgdata.replace(/^data:([A-Za-z-+/]+);base64,/, '');
+      
+      fs.writeFileSync(path, base64Data,  {encoding: 'base64'});
+
+      return res.send(path);
+
+  } catch (e) {
+      next(e);
+  }
+}
+
+app.post('/profile', (req, res, next) => {
+  uploadImage(req, res, next)
+})
 
 app.get('/animation.html', (req, res) => {
   res.sendFile(path.join(__dirname, './animation.html'));
